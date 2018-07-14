d1f1cf3af7cb7ed7abe41b6dc179f4c3f92beebf
Lisa Canini <three_nineteen@yahoo.com>
2018-07-14 09:58:18 -0700

    one q answered, get test working

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,6 +2,13 @@
 
 
 1. In Jest, what are the differences between beforeAll, afterAll, beforeEach, and afterEach? When do they run? What are they used for?
+beforeAll runs before the entire test suite is run.  This allows `something` to be established before the tests are run.  This could be used to start a server or create data that can be tested upon.
+
+afterAll runs after the entire test suite is run.  afterAll cleans up the database or closes the server connection after the tests have run.
+
+beforeEach runs before a single test is run.  It can be used to make sure that the data being tested is exactly the same and hasn't been changed by previous tests.
+
+afterEach runs after a single test is run.  If the test has made a promise, afterEach ensures that is resolved before moving on.
 1. What is the point of Test Driven Development? What do you personally think about this approach?
 1. What is a mock function? How do we use it to test a callback passed to a function?
 1. Mention three types of tests.
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-const Game = require('./games/Game');
+const server = require('./server');
+const request = require('supertest');
+const Game = require('../games/Game');
 
 describe('The API Server', () => {
   beforeAll(() => {
@@ -22,6 +23,12 @@ describe('The API Server', () => {
   // // hint - these wont be constants because you'll need to override them.
 
   beforeEach(() => {
+    let newGame = {
+      title: 'Super Mario Bros',
+      genre: 'Platform game',
+      releaseDate: 'September 13, 1985'
+    }
+    Game.create(newGame);
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
@@ -29,13 +36,29 @@ describe('The API Server', () => {
 
   afterEach(() => {
     //   // clear the games collection.
+    Game.remove({})
   });
 
   it('runs the tests', () => {});
 
   // test the POST here
-
+  it('adds a document to the database', async() => {
+    
+    const newGame = {
+      title: 'Super Mario Bros',
+      genre: 'Platform game',
+      releaseDate: 'September 13, 1985'
+    }
+    await request(server)
+      .post('/api/games', newGame)
+      .expect(201)
+  })
   // test the GET here
+  it('has a GET endpoint that return 200', async() => {
+    await request(server)
+      .get('/api/games')
+      .expect(200)
+  })
 
   // Test the DELETE here
 });
