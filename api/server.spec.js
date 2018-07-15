05a0c5a2b3a909519e239aaf6bcc5036ccfdc51f
Lisa Canini <three_nineteen@yahoo.com>
2018-07-15 11:11:04 -0700

    server testing properly, beforeEach and afterEach working correctly, post and get working

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-const Game = require('./games/Game');
+const request = require('supertest');
+let server = require('./server');
+const Game = require('../games/Game');
 
 describe('The API Server', () => {
   beforeAll(() => {
@@ -21,21 +22,54 @@ describe('The API Server', () => {
   let gameId;
   // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
+  beforeEach(async() => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
+    let mario = {
+      title: 'Super Mario Bros',
+      genre: 'Platform game',
+      releaseDate: 'September 13, 1985'
+    }
+    testGame = await Game.create(mario);
   });
 
   afterEach(() => {
     //   // clear the games collection.
+    return Game.remove()
   });
 
   it('runs the tests', () => {});
 
   // test the POST here
+  it ('should return status 201 after being saved', async() => {
+    const newGame = {
+      title: 'Duck Hunt',
+      genre: 'shooter',
+      releaseDate: 'October 18, 1985'
+    }
+    const response = await request(server)
+      .post('/api/games', newGame)
+    expect(201)
+  } )
+  it ('should return game data after being saved to db', async() => {
+    const newGame = {
+      title: 'Duck Hunt',
+      genre: 'shooter',
+      releaseDate: 'October 18, 1985'
+    }
+    const response = await request(server)
+      .post('/api/games')
+      .send(newGame)
+      expect(newGame)
+      expect(response.body.title).toEqual('Duck Hunt')
+  })
 
   // test the GET here
-
+  it('has a GET / endpoint that returns 200', async () => {
+    await request(server)
+      .get('/api/games')
+      .expect(200)
+  })
   // Test the DELETE here
 });
